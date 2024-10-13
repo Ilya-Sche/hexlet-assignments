5d3b8f99d3bb7b6b84ed7ca53a951fdbecd0965f
@hexlet/cli <support@hexlet.io>
2024-10-14 00:21:12 +0300

    submit ruby-basics-ru/oop

---
+++ ruby-basics-ru/oop/lib/url.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+# BEGIN
+require 'uri'
+require 'cgi'
+require 'forwardable'
+
+class Url
+    include Forwardable
+    def_delegators :@uri, :scheme, :host, :port
+
+  def initialize(url)
+    @uri = URI.parse(url)
+  end
+
+  def query_params
+    return {} unless @uri.query
+    CGI.parse(@uri.query).transform_values { |v| v.size > 1 ? v : v.first }
+  end
+
+  def query_param(key, default = nil)
+    query_params.fetch(key, default)
+  end
+
+  include Comparable
+  def ==(other)
+    return false unless other.is_a?(Url)
+    @uri.to_s == other.to_s
+  end
+
+  def <=>(other)
+    return nil unless other.is_a?(Url)
+    @uri.to_s <=> other.to_s
+  end
+end
+# END
