c4f36bdd875ce00710bf189138641d2fae306805
@hexlet/cli <support@hexlet.io>
2024-10-14 00:36:40 +0300

    submit ruby-basics-ru/oop

--- ruby-basics-ru/oop/lib/url.rb
+++ ruby-basics-ru/oop/lib/url.rb
@@ -15,18 +15,18 @@ class Url
   def query_params
     return {} unless @uri.query
 
-    CGI.parse(@uri.query).transform_values { |v| v.size > 1 ? v : v.first }
+    CGI.parse(@uri.query).transform_keys(&:to_sym).transform_values { |v| v.size > 1 ? v : v.first }
   end
 
   def query_param(key, default = nil)
-    query_params.fetch(key, default)
+    query_params.fetch(key.to_sym, default)
   end
 
   include Comparable
   def ==(other)
     return false unless other.is_a?(Url)
 
-    @uri.to_s == other.to_s
+    @uri.to_s == other.instance_variable_get(:@uri).to_s
   end
 
   def <=>(other)
