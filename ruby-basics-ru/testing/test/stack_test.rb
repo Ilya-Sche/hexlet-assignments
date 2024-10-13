cdf8e9f84a98f81eea31e299a0a6056734da4eef
@hexlet/cli <support@hexlet.io>
2024-10-13 15:26:58 +0300

    submit ruby-basics-ru/testing

--- ruby-basics-ru/testing/test/stack_test.rb
+++ ruby-basics-ru/testing/test/stack_test.rb
@@ -5,30 +5,33 @@ require_relative '../lib/stack'
 
 class StackTest < Minitest::Test
   # BEGIN
-    def setup
-     @stack = Stack.new
-    end
-    def test_push_element
-      @stack.push!(5) 
-      assert_equal [5], @stack.elements
-    end
-     def test_pop_element
-      @stack.push!(1) 
-      @stack.push!(2)
-      @stack.push!(3)
-      @stack.pop!
-      assert_equal [1, 2], @stack.elements
-     end
-    def test_element_delete
-      @stack.push!(1)
-      @stack.clear!
-      assert_equal [], @stack.elements
-    end
-     def test_element_empty?
-      @stack.push!(1)
-      @stack.empty?
-      assert_equal [1], @stack.elements
-    end
+  def setup
+    @stack = Stack.new
+  end
+
+  def test_push_element
+    @stack.push!(5) 
+    assert_equal [5], @stack.elements
+  end
+
+  def test_pop_element
+    @stack.push!(1) 
+    @stack.push!(2)
+    @stack.push!(3)
+    @stack.pop!
+    assert_equal [1, 2], @stack.elements
+  end
+
+  def test_element_delete
+    @stack.push!(1)
+    @stack.clear!
+    assert_equal [], @stack.elements
+  end
+
+  def test_element_empty?
+    @stack.push!(1)
+    assert !@stack.empty?
+  end
   # END
 end
 
