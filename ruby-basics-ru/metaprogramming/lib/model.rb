81ef3e5c3d259223deeb2c5ed55671f843268266
@hexlet/cli <support@hexlet.io>
2024-10-17 18:39:02 +0300

    submit ruby-basics-ru/metaprogramming

---
+++ ruby-basics-ru/metaprogramming/lib/model.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+# BEGIN
+require 'date'
+
+module Model
+
+  def initialize(attributes)
+      @attributes = attributes
+  end
+  def attribute(name, type: nil)
+      value = instance_variable_get("@#{name}")
+  return value if type.nil?
+    
+     type.parse(value)
+  end
+end
+
+
+
+class Post
+  include Model
+
+  def initialize(**kwargs)
+    kwargs.each do |attribute_name, attribute_value|
+      instance_variable_set "@#{attribute_name}", attribute_value
+      self.class.define_method attribute_name do
+        instance_variable_get("@#{attribute_name}")
+      end
+      self.class.define_method "#{attribute_name}=" do |value|
+        instance_variable_set "@#{attribute_name}", value
+      end
+    end
+  end
+end
+
+# END
+
