9c442959d7f06d518a5df4ccac7018dd3c651a46
@hexlet/cli <support@hexlet.io>
2024-10-18 15:39:19 +0300

    submit ruby-basics-ru/metaprogramming

--- ruby-basics-ru/metaprogramming/lib/user.rb
+++ ruby-basics-ru/metaprogramming/lib/user.rb
@@ -1,22 +1,10 @@
 # frozen_string_literal: true
 
-# require_relative 'model'
 
-# class Post
-#   include Model
-#   class Boolean; end
+class User
+  include Model
 
-#   attribute :id, type: Integer, default: 2
-#   attribute :title, type: String, default: "title"
-#   attribute :body, type: String, default: "body"
-#   attribute :created_at, type: DateTime, default: Date.today.to_s
-#   attribute :published, type: Boolean, default: true
-# end
-
-# class User
-#   include Model
-
-#   attribute :name, type: :string, default: 'Andrey'
-#   attribute :birthday, type: :datetime
-#   attribute :active, type: :boolean, default: false
-# end
+  attribute :name, type: :string, default: 'Andrey'
+  attribute :birthday, type: :datetime
+  attribute :active, type: :boolean, default: false
+end
