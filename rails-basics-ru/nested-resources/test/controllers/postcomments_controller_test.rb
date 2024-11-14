29054231f728c442bba5a0444102d9c65a539e75
@hexlet/cli <support@hexlet.io>
2024-11-14 19:11:56 +0300

    submit rails-basics-ru/nested-resources

--- rails-basics-ru/nested-resources/test/controllers/postcomments_controller_test.rb
+++ rails-basics-ru/nested-resources/test/controllers/postcomments_controller_test.rb
@@ -6,43 +6,43 @@ class PostcommentsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get index" do
-    get postcomments_url
+    get post_postcomments_url
     assert_response :success
   end
 
   test "should get new" do
-    get new_postcomment_url
+    get new_post_postcomment_url
     assert_response :success
   end
 
   test "should create postcomment" do
     assert_difference("Postcomment.count") do
-      post postcomments_url, params: { postcomment: { body: @postcomment.body, post_id: @postcomment.post_id } }
+      post post_postcomments_url, params: { postcomment: { body: @postcomment.body, post_id: @postcomment.post_id } }
     end
 
-    assert_redirected_to postcomment_url(Postcomment.last)
+    assert_redirected_to psot_postcomment_url(Postcomment.last)
   end
 
   test "should show postcomment" do
-    get postcomment_url(@postcomment)
+    get post_postcomment_url(@postcomment)
     assert_response :success
   end
 
   test "should get edit" do
-    get edit_postcomment_url(@postcomment)
+    get edit_post_postcomment_url(@postcomment)
     assert_response :success
   end
 
   test "should update postcomment" do
-    patch postcomment_url(@postcomment), params: { postcomment: { body: @postcomment.body, post_id: @postcomment.post_id } }
-    assert_redirected_to postcomment_url(@postcomment)
+    patch post_postcomment_url(@postcomment), params: { postcomment: { body: @postcomment.body, post_id: @postcomment.post_id } }
+    assert_redirected_to post_postcomment_url(@postcomment)
   end
 
   test "should destroy postcomment" do
     assert_difference("Postcomment.count", -1) do
-      delete postcomment_url(@postcomment)
+      delete post_postcomment_url(@postcomment)
     end
 
-    assert_redirected_to postcomments_url
+    assert_redirected_to post_postcomments_url
   end
 end
